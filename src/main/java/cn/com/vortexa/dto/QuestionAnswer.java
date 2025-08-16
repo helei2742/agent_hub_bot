aeab26b71ed6487a085e85893d2ad003294ad5ae
helei2742 <914577981@qq.com>
2025-08-17 00:55:49 +0800

    agent_hub_bot v1.0-

---
+++ src/main/java/cn/com/vortexa/dto/QuestionAnswer.java
@@ -0,0 +1,19 @@
+package cn.com.vortexa.dto;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+/**
+ * @author helei
+ * @since 2025-08-16
+ */
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class QuestionAnswer {
+    private String quizId;
+    private String questionId;
+    private String answerId;
+}
